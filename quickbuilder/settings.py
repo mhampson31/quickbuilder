622499d0454ee3813ee3aecaa851fc38bb5e6b68
Me 266144 <266144@quickbuilder.nfshost.com>
2018-12-26 17:59:53 +0000

    new settings for prod vs dev

--- quickbuilder/settings.py
+++ quickbuilder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from . import prod
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dq81_m&2k)j8+5bb=fx-x%m3%scip2ejz=j_-@1tdaa0vfa+)@'
+SECRET_KEY = prod.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,13 +74,7 @@ WSGI_APPLICATION = 'quickbuilder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {'read_default_file':'../qb.cnf',}
-    }
-}
-
+DATABASES = prod.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
