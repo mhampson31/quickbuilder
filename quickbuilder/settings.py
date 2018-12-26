c6f2e3d86e6b62c94c29e1b7f5827d7181b63bfc
Me 266144 <266144@quickbuilder.nfshost.com>
2018-12-26 17:22:16 +0000

    moving git directory down to the actual project

--- quickbuilder/settings.py
+++ quickbuilder/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'quickbuilder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {'read_default_file':'../qb.cnf',}
     }
 }
 
