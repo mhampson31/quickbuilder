92e4091a4d37159ab106f767320826d7b2d62648
Michael Hampson <toxotes+gh@gmail.com>
2018-12-28 21:46:50 -0500

    Changed faction selection on random to released factions only

--- qb/forms.py
+++ qb/forms.py
@@ -2,5 +2,8 @@ from django import forms
 from .models import Faction
 
 class RandomListForm(forms.Form):
-    faction = forms.ModelChoiceField(label="Faction", queryset=Faction.objects.all(), to_field_name='id')
+    faction = forms.ModelChoiceField(label="Faction",
+                                     queryset=Faction.objects.filter(released=True),
+                                     to_field_name='id',
+                                     empty_label='')
     threat = forms.IntegerField(label='Threat', initial=8)
